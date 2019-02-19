168f970337679cee340e77a78347a2e57f8d2e0c
OrneryCrow <ornery_crow@hotmail.com>
2019-02-19 14:00:53 -0600

    Created a NutritionPlan object in Models

---
+++ NutritionBud/NutritionBud/Models/NutritionPlan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutritionBud.Models
+{
+    public class NutritionPlan
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int PlanId { get; set; }
+
+        private static int nextId = 1;
+
+
+        public NutritionPlan()
+        {
+            PlanId = nextId;
+            nextId++;
+        }
+    }
+}
